39de3cec50e64ed550b8e059e37eb10818d328bc
went <went.out@gmail.com>
2012-06-08 12:59:52 +0400

    Update master

--- core.js
+++ core.js
@@ -8,7 +8,7 @@
             alert( 'JQ Loader Plugin Returns: ' + obj );
         };
         _js.load( './test.js', { bb: 123 }, { bb:456 }, returnCallbackStandalone );
-        $.loadSubscript( './test.js', { bb: 123 }, { bb:456 }, returnCallbackJQ );
+        $.loadSubScript( './test.js', { bb: 123 }, { bb:456 }, returnCallbackJQ );
     
     }catch(e){ alert(e); }
 })();
